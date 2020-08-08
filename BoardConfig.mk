aaf8451f165b717574a697d1f4da12404aff0fdd
Vivekachooz <vivekachooz@gmail.com>
2020-08-08 09:51:07 +0530

    lavender: Add firmware requirement
    • Also set device OTA assert
    
    Change-Id: I7ddc6c5a1422bf07bca2b90b091b7ca464e0d27e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/lavender
 
+# Assert
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := lavender
+
 # Inherit the proprietary files
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
