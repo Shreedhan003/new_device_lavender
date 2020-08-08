0e0db0d069ebddc38d4d402fc0a2d3435a078d15
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-08-08 09:51:07 +0530

    lavender: BoardConfig: Set bootloader name
    Change-Id: I7964fe9282e674b6b32bd6c13bc38851fee41a0f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,5 +10,8 @@ DEVICE_PATH := device/xiaomi/lavender
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := lavender
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm660
+
 # Inherit the proprietary files
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
