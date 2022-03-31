cb45deab78ca4175c36cb8ff67140ed741e0e9ec
Kunmun <kunmun.devroms@gmail.com>
2022-03-31 22:42:23 +0530

    lavender: Import HIDL manifest and compatibility/framework matrixes
    • HIDL manifest from LA.UM.10.2.1.r1-03400-sdm660.0
    • Point HIDL matrix to QCOM's compatiblity matrix.
    
    Change-Id: I7a24863896a7167a0ddf47c6ca711de793c985a8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,12 @@ TARGET_OTA_ASSERT_DEVICE := lavender
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
