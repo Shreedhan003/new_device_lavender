343c2c2eb41db96728618ca3a6e8ec839a148ec3
rk779 <rahulkrishna585@gmail.com>
2022-03-31 09:21:56 +0530

    lavender: BoardConfig: Import kernel config flags
    • Declare kernel revision
    • Set selinux as permissive
    • Compile kernel with SDLLVM
    
    Change-Id: I826fe563e8cfaa02634fc02e92d628296670a1ec
    Co-authored-by: Kunmun <kunmun@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,19 @@ TARGET_OTA_ASSERT_DEVICE := lavender
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 
 # Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 # Inherit the proprietary files
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -7,5 +7,8 @@
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/lavender/lavender-vendor.mk)
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
